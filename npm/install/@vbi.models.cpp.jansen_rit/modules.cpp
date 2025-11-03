1b13c49a66044bd943c30e2e76cb211f963e7c83
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-03 07:00:36 +0100

    Create modules.cpp
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ npm/install/@vbi.models.cpp.jansen_rit/modules.cpp
@@ -0,0 +1,8 @@
+vbi.models.cpp.jansen_rit	
+    vbi.models.cpp.km	
+    vbi.models.cpp.mpr	
+    vbi.models.cpp.vep	
+    vbi.models.cpp.wc	
+    vbi.models.cupy.jansen_rit	
+    vbi.models.cupy.mpr	
+    vbi.models.cupy.utils
