fd2e6877acaa5f36f4e763f7e09321c8efb33065
auraecosystem <sandraisom@icloud.com>
2026-06-10 03:19:32 +0100

    Change repository name from SERAI to neomindAI
    Signed-off-by: auraecosystem <sandraisom@icloud.com>

--- bash.sh
+++ bash.sh
@@ -9,7 +9,7 @@ git add .
 git commit -m "Initial commit: SERAI monorepo with  Aura.xlsx, AI, teleport scripts"  
 
 GITHUB_USERNAME="auraecosystem" 
-REPO_NAME="SERAI"   $auraecosystem/$neomindAI.git](https://github.com/Web4application/SERAI.git)"  
+REPO_NAME="neomindAI"   $auraecosystem/$neomindAI.git](https://github.com/Web4application/SERAI.git)"  
 PYTHON_VERSION="3.11"  
 
 git add .github/workflows/python.yml  
